c3b53a857a6fbc504e5432ffb2bcb03733df5763
rafaeldacunhaoficial <rafaeldacunhaconsultor@gmail.com>
2026-06-21 14:08:06 -0300

    fix via_varejo: use pontofrio.com.br (Ponto/ex-Via Varejo)

--- src/scrapers/via_varejo.py
+++ src/scrapers/via_varejo.py
@@ -1,4 +1,5 @@
-"""Scraper Casas Bahia (VTEX)."""
+"""Scraper Ponto (ex-Pontofrio / Via Varejo) - VTEX Intelligent Search."""
 from ._vtex_base import scrape_vtex_store
 
-def get_prices(): return scrape_vtex_store("https://www.casasbahia.com.br", "casasbahia", "Casas Bahia")
+def get_prices() -> list:
+    return scrape_vtex_store("https://www.pontofrio.com.br", "ponto", "Ponto")
